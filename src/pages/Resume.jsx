b1f4c2d8e29d81213fe5421ff2de1d26f647fc9c
samwise3 <ger7mm@virginia.edu>
2025-08-17 08:25:35 -0400

    fix: resume path update

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,4 +1,3 @@
-import { useState, Document, pdfjs } from 'react';
 import Navigation from '../components/ui/Navigation';
 import Footer from '../components/ui/Footer';
 import Header from '../components/ui/Header';
@@ -14,12 +13,12 @@ function Resume() {
         <hr />
       </header>  
       <iframe 
-        src="src/assets/Resume.pdf" 
+        src="/assets/Resume.pdf" 
         width="100%" 
         height="600px"
         type="application/pdf">
         <p>Your browser doesn't support PDFs. 
-          <a href="src/assets/Resume.pdf">Download the PDF</a>
+          <a href="/assets/Resume.pdf">Download the PDF</a>
         </p>
       </iframe>
       <Footer />
