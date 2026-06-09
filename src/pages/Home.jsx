324971b91fbcd25ed53fcaf557bc9363607fcf8a
samwise3 <ger7mm@virginia.edu>
2026-06-09 09:57:24 -0700

    updated about me

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,25 +12,23 @@ function Home() {
 
       <main>
         <div className="center">
-          <p>Hi! I'm Sam, a rising fourth-year student at the University of Virginia pursuing a double major in Computer Science and Economics. 
+          <p>Hi! I'm Sam, a 2026 graduate from the University of Virginia where I earned a BA in Computer Science and a BA Economics. 
           This interdisciplinary background has given me a unique perspective on how technology can drive meaningful change. I'm 
           particularly interested in sustainability and renewable energy, having studied sustainable development through UVA's <em>Odyssey 
           Through the Anthropocene</em> program and analyzed renewable energy companies like NextEra Energy through my involvement in the club, <em>Smart Women's Securities</em>.</p>
           <br/>
-          <p>This summer, I had the opportunity to intern at Liberty Mutual in their Boston office. As a member of their TechStart program, I worked on 
+          <p>Last summer, I had the opportunity to intern at Liberty Mutual in their Boston office. As a member of their TechStart program, I worked on 
           an agile-development team, completing full-stack deliverables using Javascript, React, and GitHub Actions. It was a fantastic way to gain insight
           into the life cycle of a software product and experience the role of a software engineer. I was able to participate in ceremonies, demoing the
-          work I completed, and collaborated with other interns to compete in the program's hackathon.
+          work I completed, and collaborated with other interns to compete in the program's hackathon. I am excited to return as a full-time software engineer starting July 2026.
           </p>          
           <br/>
-          <p>Outside of academics, I enjoy soccer, skiing, and spending time outdoors. These interests have naturally reinforced my commitment 
-          to environmental preservation and sustainable technology. As I work toward my graduation in May 2026, I'm focused on developing my 
-          software engineering skills while exploring ways to contribute to renewable energy advancement.</p>
-          <br/>
-          <p>Through my involvement with extracurriculars like the <em>Google Developers Club</em> and work as a Project Consultant at <em>Enactus</em>, I continue to build both my 
+          <p>During my time at school, my involvement with extracurriculars like the <em>Google Developers Club</em> and work as a Project Consultant at <em>Enactus</em>, I continue to build both my 
           technical expertise and practical experience in implementing sustainable solutions and collaborating on diverse teams. My time as a member
           and mentor in <em>Women in Computer Science</em> has also provided impactful opportunities for growth and connection within the field.</p>
           <br />
+          <p>Outside of academics, I enjoy soccer, skiing, mountain biking, fitness, and generally spending time outdoors. These interests have naturally reinforced my commitment
+            to environmental preservation and sustainable technology.</p>
           
           <Link className="custom-link" to="/projects">
             View my Projects</Link>
